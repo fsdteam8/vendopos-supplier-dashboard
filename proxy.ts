f0c91a3e68086a05e266202060ec33cf38d7499f
Md.Habibulla <mdhabibulla146425@gmail.com>
2026-01-28 06:51:32 +0600

    proxy file

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,26 @@
 import { withAuth } from "next-auth/middleware";
 
-export const proxy = withAuth({
-  callbacks: {
-    authorized: ({ token }) => !!token,
+export default withAuth(
+  function middleware() {
+    // optional custom logic here
   },
-});
-
-export default proxy;
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|login|forget-password|reset-your-password|verify-otp|email-verify).*)",
+    /**
+     * Protect everything EXCEPT:
+     * - api routes
+     * - next static files
+     * - next image optimizer
+     * - public assets
+     * - auth pages
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|images|icons|fonts|uploads|login|forget-password|reset-your-password|verify-otp|email-verify).*)",
   ],
 };
